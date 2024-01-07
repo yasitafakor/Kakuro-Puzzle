a1c426af2a240338b16009d10024cf2864c58cf2
yasitafakor <109222162+yasitafakor@users.noreply.github.com>
2024-01-08 01:01:23 +0330

    Update Improved_CSP.py

--- Improved_CSP.py
+++ Improved_CSP.py
@@ -5,116 +5,105 @@ class Kakuro:
             ['c2', 0, 0],
             ['c4', 0, 0]
         ]
-
+​
         self.constraints = {
-            1: {'down': 11},
-            2: {'right': 8},
-            3: {'down': 3},
-            4: {'right': 6}
+            'c1': {'down': 11},
+            'c2': {'right': 8},
+            'c3': {'down': 3},
+            'c4': {'right': 6}
         }
-
-        self.constraints_directions = {
-            1: {'down'},
-            2: {'right'},
-            3: {'down'},
-            4: {'right'}
-        }
-
-        self.constraints_target = {
-            1: {11},
-            2: {8},
-            3: {3},
-            4: {6}
-        }
-
+​
         self.constraints_location = {
-            1: [0, 1],
-            2: [1, 0],
-            3: [0, 2],
-            4: [2, 0]
+            'c1': [0, 1],
+            'c2': [1, 0],
+            'c3': [0, 2],
+            'c4': [2, 0]
         }
-
-        self.constraints_list = [1, 2, 3, 4]
-
+​
+        self.constraints_list = ['c1', 'c2', 'c3', 'c4']
+​
         self.domains = {
-            'a11': [1, 2, 3, 4, 5, 6, 7, 8, 9],
-            'a12': [1, 2, 3, 4, 5, 6, 7, 8, 9],
-            'a21': [1, 2, 3, 4, 5, 6, 7, 8, 9],
-            'a22': [1, 2, 3, 4, 5, 6, 7, 8, 9]
+            'c1': [1, 2, 3, 4, 5, 6, 7, 8, 9],
+            'c2': [1, 2, 3, 4, 5, 6, 7, 8, 9],
+            'c3': [1, 2, 3, 4, 5, 6, 7, 8, 9],
+            'c4': [1, 2, 3, 4, 5, 6, 7, 8, 9]
         }
-
+​
         self.column = 3
         self.row = 3
-
+​
     def print_board(self):
         for i in range(0, 3):
             for j in range(0, 3):
                 print(self.board[i][j], end=" ")
             print()
         print()
-
-    def test(self):
-        t = self.constraints_list.pop(0)
-        print(self.domains.get('a11'))
-        n = 4
-        print(self.domains.get('a11').remove(n))
-        print(self.domains.get('a11'))
-        print(a+1)
-
-    def get_domain(self, i, j):
-        if i == 1 and j == 1:
-            return self.domains.get('a11')
-        elif i == 1 and j == 2:
-            return self.domains.get('a12')
-        elif i == 2 and j == 1:
-            return self.domains.get('a21')
-        elif i == 2 and j == 2:
-            return self.domains.get('a22')
-
-    def get_constraint(self, row, col):
-        if row == 0 and col == 1:
-            return 1
-        if row == 1 and col == 0:
-            return 2
-        if row == 0 and col == 2:
-            return 3
-        if row == 2 and col == 0:
-            return 4
-
-    def update_domain(self, domain, num, i, j):
-
-    def solve_kakuro(self, row, col, temp_domain, temp_list):
-
-        temp_constraint = self.get_constraint(row, col)
-
-        copy_list = self.constraints_list.copy()
-
-        if self.constraints_directions.get(temp_constraint).pop() == 'down':
-            j = self.constraints_location.get(temp_constraint).pop()
-            i = self.constraints_location.get(temp_constraint).pop(0) + 1
-
-            for num in self.get_domain(i, j):
-                if self.board[i][j] != 0:
-                    self.board[i][j] = num
-                    copy_domain = self.domains.copy()
-                    self.update_domain(copy_domain, num, i, j)
-                    if solve_kakuro(row+1, col, copy_domain, copy_list):
+​
+    def update_domain(self, num, constraint_name):
+        if num in self.domains.get(constraint_name):
+            self.domains.get(constraint_name).remove(num)
+​
+    def solve(self, row, col, constraints, domain, constraint_name=None, direction=None, number=None, smm=0):
+​
+        if len(self.constraints_list) == 0:
+            return True
+​
+        if row == -1 and col == -1:
+            constraint_name = self.constraints_list.pop(0)
+            direction = list(self.constraints.get(constraint_name).keys())[0]
+            row, col = self.constraints_location.get(constraint_name)
+            if direction == 'down':
+                row += 1
+            else:
+                col += 1
+            
+            domain = self.domains.get(constraint_name)
+            if self.solve(row, col, constraints, domain, constraint_name, direction, number):
+                return True
+​
+            return False
+​
+        if direction == 'down':
+            if row == len(self.board) or self.board[row][col] == '#' or self.board[row][col] in self.constraints:
+                # send to next constraint
+                if smm != self.constraints.get(constraint_name).get(direction):
+                    return False
+                return self.solve(-1, -1, constraints, domain, constraint_name, direction, number)
+​
+            if self.board[row][col] != 0:
+                # it has been filled before
+                return self.solve(row + 1, col, self.board, constraints, domain, constraint_name, direction)
+​
+            elif self.board[row][col] == 0:
+                for i in domain:
+                    self.board[row][col] = i
+                    # self.print_board()
+                    self.update_domain(i, constraint_name)
+                    if self.solve(row + 1, col, constraints, domain, constraint_name, direction, i, smm + i):
                         return True
-
-        elif self.constraints_directions.get(temp_constraint).pop() == 'right':
-            j = self.constraints_location.get(temp_constraint).pop()
-            i = self.constraints_location.get(temp_constraint).pop(0) + 1
-
-            for num in self.get_domain(i, j):
-                if self.board[i][j] != 0:
-                    self.board[i][j] = num
-                    copy_domain = self.domains.copy()
-                    self.update_domain(copy_domain, num, i, j)
-                    if solve_kakuro(row+1, col, copy_domain, copy_list):
+                return False
+​
+        if direction == 'right':
+            if col == len(self.board) or self.board[row][col] == '#' or self.board[row][col] in self.constraints:
+                if smm != self.constraints.get(constraint_name).get(direction):
+                    return False
+                # send to next constraint
+                return self.solve(-1, -1, constraints, domain, constraint_name, direction, number)
+​
+            if self.board[row][col] != 0:
+                # it has been filled before
+                return self.solve(row, col + 1, self.board, constraints, domain, constraint_name, direction)
+​
+            elif self.board[row][col] == 0:
+                for i in domain:
+                    self.board[row][col] = i
+                    # self.print_board()
+                    self.update_domain(i, constraint_name)
+                    if self.solve(row, col + 1, constraints, domain, constraint_name, direction, i, smm + i):
                         return True
-
-
-
-
+                return False
+​
+​
 k = Kakuro()
+k.solve(-1, -1, k.constraints, k.domains)
 k.print_board()
